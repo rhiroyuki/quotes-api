f9d675341daa376508c85fbbf3d0b39d9aa3e1fd
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2019-02-12 22:00:26 -0200

    add endpoint to update a quote

--- config/routes/quotes.js
+++ config/routes/quotes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/:id', QuotesController.show);
 router.post('/', QuotesController.create);
+router.post('/update/:id', QuotesController.update);
 
 module.exports = router;
