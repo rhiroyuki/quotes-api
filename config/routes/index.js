656c858a851252fc091dc834cb7368672339fc62
Julio Fortunato <juliofortunato58@gmail.com>
2019-02-13 19:26:31 -0200

    Add Swagger doc
    Adds Swagger to serve as interactive documentation for the API endpoints

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const quotes = require('./quotes');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 
 const router = express.Router();
 
+router.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 router.use('/quotes', quotes);
 
 module.exports = router;
