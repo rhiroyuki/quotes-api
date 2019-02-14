4298b6eb8a376d3e1fe062ef3ef434d6f9cfae93
Julio Fortunato <juliofortunato58@gmail.com>
2019-02-14 19:09:59 -0200

    Add an endpoint for deleting a quote

--- config/routes/quotes.js
+++ config/routes/quotes.js
@@ -7,5 +7,6 @@ router.get('/', QuotesController.list);
 router.get('/:id', QuotesController.show);
 router.post('/', QuotesController.create);
 router.put('/:id', QuotesController.update);
+router.delete('/:id', QuotesController.delete);
 
 module.exports = router;
